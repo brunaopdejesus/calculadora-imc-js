9a5f3b439ea1bfb4c4dcdb726d1a0a91dd80d4cf
BRUNA OLIVEIRA PEDROSO DE JESUS <21192918@CFP127ALUNO.LOCAL>
2021-08-17 10:45:53 -0300

    cursor pointer

--- exemplo.js
+++ exemplo.js
@@ -1,14 +1,52 @@
-function calcularImc() {
+function camposValidos() {
     const nome = document.getElementById("nome").value;
     const altura = parseFloat(document.getElementById("altura").value);
     const peso = parseFloat(document.getElementById("peso").value);
 
-    // "" = vazio
-    if (nome.trim() !== "" && altura !== NaN && peso !== NaN) {
-        const imc = peso / (altura ** 2);
+    return (nome.trim() !== "" && !isNaN(altura) && !isNaN(peso));
+}
+
+function classificarImc (imc) {
+    let classificacao = "";
+    if (imc < 18.5) {
+        classificacao = "abaixo do peso.";
+    } else if (imc < 25) {
+        classificacao = "com peso ideal. Parabéns!";
+    } else if (imc < 30) {
+        classificacao = "levemente acima do peso.";
+    } else if (imc < 35) {
+        classificacao = "com obesidade grau I.";
+    } else if (imc < 40) {
+        classificacao = "com obesidade grau II.";
     } else {
-        alert ("Preencha todos os campos!");
+        classificacao = "com obesidade grau III.";
+    } 
+
+    return classificacao;
+}
+
+function calcularImc (peso, altura) {
+    return (peso / (altura ** 2)).toFixed(1);
+}
+
+function exibirImc() {
+    const nome = document.getElementById("nome").value;
+    const altura = parseFloat(document.getElementById("altura").value);
+    const peso = parseFloat(document.getElementById("peso").value);
+    const resultado = document.getElementById("resultado");
+
+    // "" = vazio
+    if (camposValidos()) {
+
+        const imc = calcularImc (peso, altura);
+
+        const classificacao = classificarImc(imc);
+
+        resultado.textContent = `${nome}, seu IMC é ${imc} e você está ${classificacao}`;
+    
+    } else {
+        resultado.textContent = "Preencha todos os campos!";
     }
 }
 
-document.getElementById("calcular").addEventListener("click", calcularImc);
+document.getElementById("calcular").addEventListener("click", exibirImc());
